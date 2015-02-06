2b02b0ce12d0e55bb3ec58e78ec96b7b4e81ef96
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-02-06 17:50:47 +0100

    Fixed default example

--- examples/default.pp
+++ examples/default.pp
@@ -1,5 +1,5 @@
 #
-# sudo puppet apply /vagrant/modules/puppet-symfony/example/default.pp
+# sudo puppet apply /vagrant/modules/puppet-symfony/examples/default.pp
 # sudo puppet apply -e 'include symfony'
 #
 
