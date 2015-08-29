255a76231df400852594440abfe69dfd83670933
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-08-29 17:51:06 +0200

    Updated default Symfony Standard to 2.7.3/Full.5

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,13 +10,13 @@ class symfony::params {
     $withNodejs          = true
     $withAllPhars        = true
     $withComposerInstall = true
+    $repo                = 'https://github.com/by-examples/symfony-standard.git'
+    $branch              = '2.7.3/Full.5'
     $withPhpMyAdmin      = true
     $withRabbitMQ        = true
     $rabbitMQHost        = 'rabbit_mq_host'
     $rabbitMQUser        = 'RabbitMQUser'
     $rabbitMQPassword    = 'passR-A-B-B-I-T123'
-    $repo                = 'https://github.com/by-examples/symfony-standard.git'
-    $branch              = '2.6.4/Full.6'
     $withSelenium        = true
     $withRedis           = true
     $withDEVSettings     = true
